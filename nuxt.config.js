15d2009138247487f261fd2e2ae65d3c0cc75db6
Collins Ruto <borecollins@gmail.com>
2020-08-29 01:08:48 +0300

    Deploy to Google cloud run

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,13 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from "@nuxt/content-theme-docs";
 
-export default theme({})
+export default theme({
+  server: {
+    port: 5000, // default: 3000
+    host: "0.0.0.0", // default: localhost,
+    timing: false,
+  },
+  target: "server",
+  content: {
+    dir: "content",
+  },
+});
